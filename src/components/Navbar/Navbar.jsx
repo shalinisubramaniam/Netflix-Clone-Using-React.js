792643f543552594204cf00bc21c3c1cb63700a5
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-05-24 20:51:33 +0530

    implemented logo on home page

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+      </div>
+      <div className="navbar-right">
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
 
 const Home = () => {
   return (
     <div className='home'>
-      
+      <Navbar/>
     </div>
   )
 }

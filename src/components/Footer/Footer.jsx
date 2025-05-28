89e549ec76c7df692ff8dfd3674a9f2c1b301032
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-05-28 21:27:11 +0530

    added other footer components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,6 +13,21 @@ const Footer = () => {
         <img src={instagram_icon} alt="" />
         <img src={facebook_icon} alt="" />
       </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Centre</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookies Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text'>© 1997-2025 Netflix, Inc.</p>
     </div>
   )
 }

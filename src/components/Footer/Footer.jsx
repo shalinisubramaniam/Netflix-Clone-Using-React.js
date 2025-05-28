31121bdaec3413b7d66928719c8c0c38910f44be
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-05-28 21:18:13 +0530

    added footer icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,18 @@
 import './Footer.css'
+import youtube_icon from '../..//assets/youtube_icon.png'
+import twitter_icon from '../..//assets/twitter_icon.png'
+import instagram_icon from '../..//assets/instagram_icon.png'
+import facebook_icon from '../..//assets/facebook_icon.png'
 
 const Footer = () => {
   return (
     <div className='footer'>
-      
+      <div className="footer-icons">
+        <img src={youtube_icon} alt="" />
+        <img src={twitter_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={facebook_icon} alt="" />
+      </div>
     </div>
   )
 }

b067e180eab5305b0774307ec1b962809aedf80b
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-05-30 13:17:54 +0530

    styled login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,16 @@
+import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 
 const Login = () => {
+
+  const [signState, setSignState] = useState("Sign In")
+
   return (
     <div className='login'>
       <img src = {logo} className='login-logo' alt="" />
-      <div className="login-form"></div>
-      <h1>Sign In</h1>
+      <div className="login-form">
+      <h1>{signState}</h1>
       <form>
         <input type="text" placeholder='Your name' />
         <input type="email" placeholder='Email' />
@@ -20,6 +24,11 @@ const Login = () => {
           <p>Need Help?</p>
         </div>
       </form>
+      <div className="form-switch">
+        <p>New to Netflix? <span>Sign Up Now</span></p>
+        <p>Already have an account? <span>Sign In Now</span></p>
+      </div>
+      </div>
     </div>
   )
 }

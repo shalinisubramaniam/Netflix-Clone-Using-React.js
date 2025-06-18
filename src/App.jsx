93f5a8f56eb69775ba37bc79b97dc6a94bea02bd
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-06-18 20:12:41 +0530

    installed and implemented toastify

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Player from './pages/Player/Player'
 import { onAuthStateChanged } from 'firebase/auth'
 import { useEffect } from 'react'
 import { auth } from './firebase'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
 
@@ -24,6 +25,7 @@ const App = () => {
 
   return (
     <div>
+      <ToastContainer theme='dark' />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBChrGfmWAjaBwLLAZ17iEu-z4DE593XU4",
@@ -28,7 +29,7 @@ const signup = async (name, email, password) => {
     }
     catch(error) {
         console.log(error);
-        alert(error);
+        toast.error(error.code);
     }
 }
 
@@ -37,7 +38,7 @@ const login = async(email, password)=>{
         await signInWithEmailAndPassword(auth, email, password);
     } catch (error) {
         console.log(error);
-        alert(error);
+        toast.error(error.code);
     }
 }
 
